772ad4f7cf72a94ef1002c03f75099fc6369430f
HendrikJohannesCruijff <qjtjzj2733@naver.com>
2023-08-16 14:28:21 +0900

    test2 추가

--- Main.java
+++ Main.java
@@ -9,7 +9,9 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("test ");
+
+        System.out.println("test2");
+        
     }
 }
